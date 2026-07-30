853352f73174afda86da41412c49cae9357f5bd5
Ahvabi123 <ahvabi123@gmail.com>
2026-07-30 23:37:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,17 +18,26 @@ else:
     st.write(f"Great! You selected: {v3}")
 
 if st.button("Find Country"):
- st.write("Button works!")
+ results = []
  for country in countries:
      score = 0
      if v1 >= country["recommended_budget"]:
       score += 40
+
      if v2 >= country["flight_time"]:
         score += 30
+
      for purpose in v3:
       if purpose in country["purpose"]:
             score += 30
-            st.write(f"🌍 Country: {country['name']}")
-            st.write(f"💰 Recommended Budget: €{country['recommended_budget']}")
-            st.write(f"✈️ Flight Time: {country['flight_time']} hours")
-            st.write(f"🎯 Purposes: {', '.join(country['purpose'])}")
+            break
+     results.append({
+    "name": country["name"],
+    "score": score
+})
+
+     st.write(f"🌍 Country: {country['name']}")
+     st.write(f"💰 Recommended Budget: €{country['recommended_budget']}")
+     st.write(f"✈️ Flight Time: {country['flight_time']} hours")
+     st.write(f"🎯 Purposes: {', '.join(country['purpose'])}")
+     st.write(results)
